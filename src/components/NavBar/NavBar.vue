ef37afa5d64322020193c9b69b3023ab695744bd
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-09-19 16:16:11 -0300

    First commit

---
+++ src/components/NavBar/NavBar.vue
@@ -0,0 +1,52 @@
+<script setup>
+import Switch from '../Switch/Switch.vue';
+</script>
+
+<template>
+    <nav class="bg-white  border-gray-200 dark:bg-gray-900">
+        <div class="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto p-4">
+            <a href="https://flowbite.com" class="flex items-center">
+                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">LD</span>
+            </a>
+            <div id="mega-menu" class="items-center justify-between hidden w-full md:flex md:w-auto">
+                <ul class="flex flex-col mt-4 font-medium md:flex-row md:space-x-8 md:mt-0">
+                    <li>
+                        <a
+                            href="#"
+                            class="block py-2 pl-3 pr-4 text-blue-600 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-blue-500 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
+                            aria-current="page"
+                            >Home</a
+                        >
+                    </li>
+                    <li>
+                        <button
+                            id="mega-menu-dropdown-button"
+                            data-dropdown-toggle="mega-menu-dropdown"
+                            class="flex items-center justify-between w-full py-2 pl-3 pr-4 font-medium text-gray-900 border-b border-gray-100 md:w-auto hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
+                        >
+                            About
+                        </button>
+                    </li>
+                    <li>
+                        <a
+                            href="#"
+                            class="block py-2 pl-3 pr-4 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
+                            >Portfólio</a
+                        >
+                    </li>
+                    <li>
+                        <a
+                            href="#"
+                            class="block py-2 pl-3 pr-4 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
+                            >Contato</a
+                        >
+                    </li>
+                </ul>
+            </div>
+
+            <div>
+                <Switch />
+            </div>
+        </div>
+    </nav>
+</template>
---
+++ src/views/Header/Header.vue
@@ -0,0 +1,11 @@
+<script setup>
+import NavBar from '../../components/NavBar/NavBar.vue';
+</script>
+
+<template>
+    
+    <header>
+        <NavBar />
+    </header>
+    
+</template>
