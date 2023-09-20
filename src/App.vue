dd07ff96e37982bda4f887edbfaa50442be017f0
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2023-09-20 13:17:02 -0300

    Estrutura principal criada

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,16 @@
 <script setup>
-import HeaderVue from './views/Header/Header.vue';
+import HeaderVue from '@/views/Header/Header.vue';
+import About from '@/views/About/About.vue';
 </script>
 
 <template>
-  <div>
+  <div class="w-full h-full">
     <HeaderVue />
+
+    <About />
+
+    <!-- <main>
+
+    </main> -->
   </div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
