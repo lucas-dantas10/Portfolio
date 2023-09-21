adf91e36aca322d5927b106524a75157694e2c97
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-09-21 12:23:34 -0300

    Finished component of About Me

--- src/views/About/About.vue
+++ src/views/About/About.vue
@@ -1,44 +1,29 @@
+<script setup>
+import AboutImage from "@/assets/imgs/about_me.svg";
+</script>
 <template>
-    <section class="flex flex-wrap items-center gap-16 max-w-screen-xl h-[80vh] mx-auto p-4">
-        <div class="flex flex-col gap-4 items-center">
-            <div
-                class="border-l-gray-950 rounded-full px-4 py-2 bg-gray-950 hover:bg-blue-600 dark:bg-gray-950 text-white dark:hover:bg-white dark:hover:text-gray-950"
-            >
-                hi
-            </div>
-            <div
-                class="border-l-gray-950 rounded-full px-4 py-2 bg-gray-950 hover:bg-blue-600 dark:bg-gray-950 text-white dark:hover:bg-white dark:hover:text-gray-950"
-            >
-                oi
-            </div>
-            <div
-                class="border-l-gray-950 rounded-full px-4 py-2 bg-gray-950 hover:bg-blue-600 dark:bg-gray-950 text-white dark:hover:bg-white dark:hover:text-gray-950"
-            >
-                oi
+    <section class="w-full h-screen" id="about">
+        <div class="flex items-center justify-center mt-32 mb-16">
+            <div class="w-[6rem]">
+                <h1 class="text-center text-4xl font-semibold dark:text-white border-b-2 border-blue-600 pb-2">
+                    Sobre
+                </h1>
             </div>
         </div>
 
-        <div class="flex-1 flex flex-col gap-4 dark:text-white">
-            <h1 class="dark:text-white text-5xl font-semibold">Lucas Dantas</h1>
+        <div class="w-full h-full flex justify-center gap-32 2xl:justify-between">
+            <div class="w-80 h-80">
+                <img :src="AboutImage" alt="Imagem de um Vector Pensativo" class="w-80- h-80" />
+            </div>
 
-            <h2 class="text-2xl font-semibold">Desenvolvedor Full Stack</h2>
+            <div class="w-[40%] flex flex-col gap-4">
+                <h2 class="text-2xl font-bold dark:text-blue-600">Desenvolvedor Full Stack</h2>
 
-            <p class="text-lg font-medium">Este é meu Portfólio Oficial.</p>
-
-            <div class="flex flex-col w-[20%] gap-2">
-                <div class="flex items-center gap-4 border-b pb-2">
-                    <p>html</p>
-                    <p>css</p>
-                    <p>javascript</p>
-                </div>
-
-                <div>
-                    <button
-                        class="uppercase font-semibold py-2 px-4 rounded-md border-2 border-gray-900 hover:bg-gray-900 hover:text-white dark:bg-white text-black dark:hover:bg-gray-950 dark:hover:text-white"
-                    >
-                        Download cv
-                    </button>
-                </div>
+                <p class="text-justify font-medium dark:text-white">
+                    Cursando Sistemas de Informação, Desenvolvedor Full Stack. Foco em implementar, executar,
+                    integrar desenvolvendo sistemas, sites, softwares e outros. Sou extremamente motivado, e sempre
+                    estou em constante desenvolvimento de minhas habilidades.
+                </p>
             </div>
         </div>
     </section>
