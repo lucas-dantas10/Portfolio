3f3a06350644b90c5a470a16e78449f59f88f734
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-09-21 10:05:42 -0300

    Add component of Header

--- src/App.vue
+++ src/App.vue
@@ -7,18 +7,3 @@ import HeaderVue from './views/Header/Header.vue';
     <HeaderVue />
   </div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
