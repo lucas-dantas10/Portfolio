cb37e4751afe8323dcf2d2d05695cfe20ded06d4
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-09-22 17:04:19 -0300

    Component of Contact,  structure finished

---
+++ src/views/Contact/Contact.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="w-full h-screen mt-32" id="contact">
+        <div class="flex items-center justify-center mt-32 mb-16">
+            <div class="w-[8rem]">
+                <h1 class="text-center text-4xl font-semibold dark:text-white border-b-2 border-blue-600 pb-2">
+                    Contato
+                </h1>
+            </div>
+        </div>
+
+        <div class="w-full flex items-center justify-between">
+            <div class="flex items-center gap-6">
+                <h1 class="dark:text-white">icone</h1>
+
+                <div class="flex flex-col justify-center dark:text-white">
+                    <h4 class="text-2xl font-medium">Localização:</h4>
+                    <p>Rio de Janeiro - RJ</p>
+                </div>
+            </div>
+
+            <div class="flex items-center gap-6">
+                <h1 class="dark:text-white">icone</h1>
+
+                <div class="flex flex-col justify-center dark:text-white">
+                    <h4 class="text-2xl font-medium">E-mail:</h4>
+                    <p>lucas.dantas.nogueira@gmail.com</p>
+                </div>
+            </div>
+
+            <div class="flex items-center gap-6">
+                <h1 class="dark:text-white">icone</h1>
+
+                <div class="flex flex-col justify-center dark:text-white">
+                    <h4 class="text-2xl font-medium">Telefone:</h4>
+                    <p>+55 (21) 99596-9988</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
