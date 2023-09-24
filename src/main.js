09c74361e72ad28cd840f4f96c34815e6c8e00ed
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-09-24 14:04:40 -0300

    Adicionado ícones para a aplicação

--- src/main.js
+++ src/main.js
@@ -2,4 +2,24 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+// ÍCONES
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import { 
+    BiGithub, BiLinkedin, BiInstagram, ViFileTypeHtml, ViFileTypeCss, CoJavascript, ViFileTypePhp3, 
+    ViFileTypeVue, SiLaravel, ViFileTypeDocker, ViFileTypeTailwind, ViFileTypePython, CoLocationPin,
+    MdEmail, CoPhone, GiWorld 
+ } 
+from "oh-vue-icons/icons";
+
+addIcons(BiGithub, BiInstagram, BiLinkedin, ViFileTypeHtml, 
+    ViFileTypeCss, CoJavascript, ViFileTypePhp3, 
+    ViFileTypeVue, SiLaravel, ViFileTypeDocker, 
+    ViFileTypeTailwind, ViFileTypePython, CoLocationPin, 
+    MdEmail, CoPhone, GiWorld, GiWorld
+);
+
+const app = createApp(App);
+
+app.component("v-icon", OhVueIcon);
+
+app.mount('#app');
