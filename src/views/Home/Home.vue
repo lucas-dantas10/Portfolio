ba74617d94a702eaddf66439d4458c6f99e72171
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-09-24 14:06:45 -0300

    Adicionado ícones corretos e hrefs corretos

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -5,21 +5,27 @@ import Lucas from '@/assets/imgs/lucas.png';
 <template>
     <section class="flex flex-wrap items-center gap-16 max-w-screen-xl h-[80vh] mx-auto p-4" id="home">
         <div class="flex flex-col gap-4 items-center">
-            <div
-                class="border-l-gray-950 rounded-full px-4 py-2 bg-gray-950 hover:bg-blue-600 dark:bg-gray-950 text-white dark:hover:bg-white dark:hover:text-gray-950"
+            <a
+                href="https://github.com/lucas-dantas10"
+                target="_blank"
+                class="border-l-gray-950 rounded-xl px-4 py-2 bg-gray-950 hover:bg-blue-600 dark:bg-gray-950 text-white dark:hover:bg-white dark:hover:text-gray-950"
             >
-                hi
-            </div>
-            <div
-                class="border-l-gray-950 rounded-full px-4 py-2 bg-gray-950 hover:bg-blue-600 dark:bg-gray-950 text-white dark:hover:bg-white dark:hover:text-gray-950"
+                <v-icon name="bi-github" :scale="1.4" animation="wrench" :hover="true"></v-icon>
+            </a>
+            <a
+                href="https://www.linkedin.com/in/lucas-dantas10/"
+                target="_blank"
+                class="border-l-gray-950 rounded-xl px-4 py-2 bg-gray-950 hover:bg-blue-600 dark:bg-gray-950 text-white dark:hover:bg-white dark:hover:text-gray-950"
             >
-                oi
-            </div>
-            <div
-                class="border-l-gray-950 rounded-full px-4 py-2 bg-gray-950 hover:bg-blue-600 dark:bg-gray-950 text-white dark:hover:bg-white dark:hover:text-gray-950"
+                <v-icon name="bi-linkedin" :scale="1.4" animation="wrench" :hover="true"></v-icon>
+            </a>
+            <a
+                href="https://github.com/lucas-dantas10"
+                target="_blank"
+                class="border-l-gray-950 rounded-xl px-4 py-2 bg-gray-950 hover:bg-blue-600 dark:bg-gray-950 text-white dark:hover:bg-white dark:hover:text-gray-950"
             >
-                oi
-            </div>
+                <v-icon name="bi-instagram" :scale="1.4" animation="wrench" :hover="true"></v-icon>
+            </a>
         </div>
 
         <div class="flex-1 flex items-center gap-40">
@@ -31,10 +37,16 @@ import Lucas from '@/assets/imgs/lucas.png';
                 <p class="text-lg font-medium">Este é meu Portfólio Oficial.</p>
 
                 <div class="w-full flex flex-col gap-4">
-                    <div class="flex items-center gap-4 border-b w-[50%] pb-2">
-                        <p>html</p>
-                        <p>css</p>
-                        <p>javascript</p>
+                    <div class="flex items-center gap-4 border-b border-gray-500 w-[80%] pb-2 dark:border-gray-400">
+                        <v-icon name="si-laravel" fill="red" :scale="1.4" animation="wrench" :hover="true"></v-icon>
+                        <v-icon name="vi-file-type-vue" :scale="1.4" animation="wrench" :hover="true"></v-icon>
+                        <v-icon name="vi-file-type-php3" :scale="1.4" animation="wrench" :hover="true"></v-icon>
+                        <v-icon name="vi-file-type-python" :scale="1.4" animation="wrench" :hover="true"></v-icon>
+                        <v-icon name="co-javascript" fill="yellow" :scale="1.4" animation="wrench" :hover="true"></v-icon>
+                        <v-icon name="vi-file-type-html" :scale="1.4" animation="wrench" :hover="true"></v-icon>
+                        <v-icon name="vi-file-type-css" :scale="1.4" animation="wrench" :hover="true"></v-icon>
+                        <v-icon name="vi-file-type-docker" :scale="1.4" animation="wrench" :hover="true"></v-icon>
+                        <v-icon name="vi-file-type-tailwind" :scale="1.4" animation="wrench" :hover="true"></v-icon>
                     </div>
 
                     <div class="w-full">
