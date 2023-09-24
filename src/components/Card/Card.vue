2ed1cc89ee2f4c72206ddfbebe169b05950f535a
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-09-24 14:05:47 -0300

    Adicionada duas props necessárias e atualização de layout do card

--- src/components/Card/Card.vue
+++ src/components/Card/Card.vue
@@ -2,7 +2,9 @@
 const props = defineProps({
     title: String,
     subtitle: String,
-    image: Image
+    image: Image,
+    github: String,
+    website: String
 });
 </script>
 
@@ -20,27 +22,24 @@ const props = defineProps({
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
                 {{ subtitle }}
             </p>
-            <a
-                href="#"
-                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-                Read more
-                <svg
-                    class="w-3.5 h-3.5 ml-2"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 14 10"
+
+            <div class="mt-4 flex items-center gap-4">
+                <a 
+                    :href="github"
+                    target="_blank"
+                    class="border-l-gray-950 rounded-md px-4 py-2 bg-gray-950 hover:bg-blue-600 dark:bg-gray-900 text-white dark:hover:bg-white dark:hover:text-gray-950"
                 >
-                    <path
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M1 5h12m0 0L9 1m4 4L9 9"
-                    />
-                </svg>
-            </a>
+                    <v-icon name="bi-github" :scale="1.4" animation="float" :hover="true"></v-icon>
+                </a>
+
+                <a 
+                    :href="website"
+                    target="_blank"
+                    class="border-l-gray-950 rounded-md px-4 py-2 bg-gray-950 hover:bg-blue-600 dark:bg-gray-900 text-white dark:hover:bg-white dark:hover:text-gray-950"
+                >
+                    <v-icon name="gi-world" class="dark:text-blue-600" :scale="1.4" animation="float" :hover="true"></v-icon>
+                </a>
+            </div>
         </div>
     </div>
 </template>
