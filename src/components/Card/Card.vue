67f91064ecc7e76738c09941cf297aa611dc932e
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-10-10 15:33:31 -0300

    Retirando imagem do projeto como um link

--- src/components/Card/Card.vue
+++ src/components/Card/Card.vue
@@ -10,9 +10,9 @@ const props = defineProps({
 
 <template>
     <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
+        <div>
             <img class="rounded-t-lg aspect-video" :src="image" alt="" />
-        </a>
+        </div>
         <div class="p-5">
             <a href="#">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
