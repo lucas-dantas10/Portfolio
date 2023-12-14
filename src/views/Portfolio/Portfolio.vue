fe84c69ebbefefbfbb10dfa7cc96236d3eb9f009
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-12-14 10:32:55 -0300

    :recycle: refactor: alterado ordem dos projetos

--- src/views/Portfolio/Portfolio.vue
+++ src/views/Portfolio/Portfolio.vue
@@ -17,15 +17,6 @@ import Ecommerce from '@/assets/imgs/ecommerce.png';
 
         <div class="w-full flex flex-wrap items-center gap-6">
             <Card
-                title="Upload Ai Website"
-                subtitle="Upload AI - Aplicação que possibilita realizar upload de videos e por meio de IA, criar automaticamente títulos chamativos e descrições com um boa indexação. Aplicação realizada em conjunto da Rocketseat."
-                :image="UploadAi"
-                github="https://github.com/lucas-dantas10/upload.ai"
-                website="https://github.com/lucas-dantas10/upload.ai"
-                :icon-language="[{name: 'vi-file-type-vue'}, {name: 'vi-file-type-typescript-official'}, {name: 'si-prisma'}, {name: 'vi-file-type-node'}]"
-            />
-
-            <Card
                 title="ToDo List Website"
                 subtitle="Todo List - Aplicação que possibilita adicionar tarefas, editar tarefas, adicionar customizações, adicionar prioridade da tarefa e adicionar categorias para as tarefas. Aplicação para poder gerenciar suas tarefas diárias."
                 :image="TodoList"
@@ -45,11 +36,20 @@ import Ecommerce from '@/assets/imgs/ecommerce.png';
 
             <Card
                 title="E-commerce (Em Desenvolvimento)"
-                subtitle="E-commerce -  Aplicação E-commerce construída com Laravel, VueJs, tailwindcss. Com painel de administrador com dashboard e relatórios, Api RESTful, gerenciamento de cliente, pagamento e etc."
+                subtitle="E-commerce -  Aplicação com painel de administrador com dashboard e relatórios, Api RESTful e site de vendas para ocorrer as vendas dos items."
                 :image="Ecommerce"
                 github="https://github.com/lucas-dantas10/e-commerce"
                 website="https://github.com/lucas-dantas10/e-commerce"
                 :icon-language="[{name: 'si-laravel', color: 'red'}, {name: 'vi-file-type-vue'}, {name: 'vi-file-type-tailwind'}, {name: 'vi-file-type-docker'}]"
+            />
+
+            <Card
+                title="Upload Ai Website"
+                subtitle="Upload AI - Aplicação que possibilita realizar upload de videos e por meio de IA, criar automaticamente títulos chamativos e descrições com um boa indexação. Aplicação realizada em conjunto da Rocketseat."
+                :image="UploadAi"
+                github="https://github.com/lucas-dantas10/upload.ai"
+                website="https://github.com/lucas-dantas10/upload.ai"
+                :icon-language="[{name: 'vi-file-type-vue'}, {name: 'vi-file-type-typescript-official'}, {name: 'si-prisma'}, {name: 'vi-file-type-node'}]"
             />
         </div>
     </section>
