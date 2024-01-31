31dc8066089cd46c3c4212daa73ea5b82311886b
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-01-31 03:52:45 -0300

    feat: add ícone wpp

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import { OhVueIcon, addIcons } from "oh-vue-icons";
 import { 
     BiGithub, BiLinkedin, CoLeetcode, ViFileTypeHtml, ViFileTypeCss, CoJavascript, ViFileTypePhp3, 
     ViFileTypeVue, SiLaravel, ViFileTypeDocker, ViFileTypeTailwind, ViFileTypePython, CoLocationPin,
-    MdEmail, CoPhone, GiWorld, BiArrowUpCircleFill, ViFileTypeTypescriptOfficial, SiPrisma, ViFileTypeNode, ViFileTypeCsharp2
+    MdEmail, CoPhone, GiWorld, BiArrowUpCircleFill, ViFileTypeTypescriptOfficial, SiPrisma, ViFileTypeNode, ViFileTypeCsharp2, BiWhatsapp
  } 
 from "oh-vue-icons/icons";
 
@@ -16,7 +16,7 @@ addIcons(
     ViFileTypeCss, CoJavascript, ViFileTypePhp3, 
     ViFileTypeVue, SiLaravel, ViFileTypeDocker, 
     ViFileTypeTailwind, ViFileTypePython, CoLocationPin, 
-    MdEmail, CoPhone, GiWorld, GiWorld, BiArrowUpCircleFill, ViFileTypeTypescriptOfficial, SiPrisma, ViFileTypeNode, ViFileTypeCsharp2
+    MdEmail, CoPhone, GiWorld, GiWorld, BiArrowUpCircleFill, ViFileTypeTypescriptOfficial, SiPrisma, ViFileTypeNode, ViFileTypeCsharp2, BiWhatsapp
 );
 
 const app = createApp(App);
