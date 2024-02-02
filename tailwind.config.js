f9b0e6ed9c8d537b34543e2a5d28449a07e15e52
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-02-02 15:37:45 -0300

    feat: add animação

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class',  
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js"
-  ],
-  theme: {
-    extend: {
-        keyframes: {
-            "fade-in-down": {
-              from: {
-                transform: "translateY(-0.75rem)",
-                opacity: "0",
-              },
-              to: {
-                transform: "translateY(0rem)",
-                opacity: "1",
-              },
+    darkMode: "class",
+    content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}", "./node_modules/flowbite/**/*.js"],
+    theme: {
+        extend: {
+            keyframes: {
+                "fade-in-down": {
+                    from: {
+                        transform: "translateY(-0.75rem)",
+                        opacity: "0",
+                    },
+                    to: {
+                        transform: "translateY(0rem)",
+                        opacity: "1",
+                    },
+                },
+                'slide-in-right': {
+                    '0%': { transform: 'translateX(-100%)' },
+                    '100%': { transform: 'translateX(0)' },
+                },
+                'slide-in-left': {
+                    '0%': { transform: 'translateX(100%)' },
+                    '100%': { transform: 'translateX(0)' },
+                },
             },
-          },
-    
-          animation: {
-            'fade-in-down': 'fade-in-down .2s ease-in-out both',
-          },
-    },
-  },
-  plugins: [
-    require("flowbite/plugin")
-  ],
-}
 
+            animation: {
+                "fade-in-down": "fade-in-down .2s ease-in-out both",
+                'slide-in-right': 'slide-in-right 1.5s ease',
+                'slide-in-left': 'slide-in-left 1.5s ease',
+            },
+        },
+    },
+    plugins: [require("flowbite/plugin")],
+};
