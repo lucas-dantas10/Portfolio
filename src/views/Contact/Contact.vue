e087b42ec33c9ca6c4304439a4dbfaa3c8d33e15
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-03-08 15:36:50 -0300

    refactor: change color of text of label

--- src/views/Contact/Contact.vue
+++ src/views/Contact/Contact.vue
@@ -61,14 +61,14 @@ function sendEmail() {
             data-aos="fade-right" data-aos-delay="100">
             <form @submit.prevent="sendEmail" class="space-y-8 w-full">
                 <div>
-                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Seu
+                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Seu
                         E-mail</label>
                     <input type="email" id="email" v-model.trim="email"
                         class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                         placeholder="name@example.com" required>
                 </div>
                 <div class="sm:col-span-2">
-                    <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Sua
+                    <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Sua
                         Mensagem</label>
                     <textarea id="message" rows="6" v-model.trim="message"
                         class="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
